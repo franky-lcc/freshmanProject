ce186020be71aa8a83c90ea9440a84b07fac23eb
franky-lcc <20024691d@connect.polyu.hk>
2020-10-11 23:48:02 +0800

    python test code

---
+++ VS python.py
@@ -0,0 +1,16 @@
+print("hello,world!")
+
+print("hello, my name is Ken")
+
+#a = 10
+#b = 220
+#c = a * b
+#print ("print c value:")
+#print (c)
+
+def multiplication(a, b):
+    c = a * b + 10
+    print("print the value from function multuiplication")
+    print(c)
+
+multiplication(3,8)
